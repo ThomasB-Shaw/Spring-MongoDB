b9af617a90c3eda58bf336d07be76cedb564118b
Thomas Brookshaw <tbrookshaw13@gmail.com>
2021-01-30 18:45:50 -0600

    Cleaned up code

--- demo/src/main/java/com/example/demo/Customer.java
+++ demo/src/main/java/com/example/demo/Customer.java
@@ -11,22 +11,6 @@ public class Customer {
     public String firstName;
     public String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public Customer() {}
 
     public Customer(String firstName, String lastName) {
